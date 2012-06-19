29e8da3d0e555560ad839e1265146aa7bd62c067
Cyril Mengin <cyril@fc-designs.com>
2012-06-19 18:14:56 +0200

    Added the autoloader for iceberg.

---
+++ lib/iceberg/Autoloader.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace iceberg;
+
+class NoAutoloaderException extends \Exception { }
+class NamespaceNotSetException extends \Exception { }
+class ClassNotFoundException extends \Exception { }
+
+class Autoloader {
+
+	private static $namespaces = array();
+	private static $registered = false;
+
+	public static function register() {
+		if (static::$registered) return true;
+
+		$set = spl_autoload_register(__CLASS__."::loadClass");
+
+		if (!$set)
+			throw new NoAutoloaderException("Unable to set the Iceberg autoloader.");
+		else
+			static::$registered = true;
+		
+		return true;
+	}
+
+	public static function addNamespace($namespace, $root) {
+		if (!static::$registered) static::register();
+
+		static::$namespaces[$namespace] = rtrim($root, DIRECTORY_SEPARATOR);
+	}
+
+	public static function loadClass($class) { 
+		$pathBits = explode("\\", $class);
+		$namespace = $pathBits[0];
+		
+		if (!array_key_exists($namespace, static::$namespaces))
+			throw new NamespaceNotSetException("Namespace \"{$pathBits[0]}\" has not been set.");
+
+		$path = static::$namespaces[$namespace]
+		       .DIRECTORY_SEPARATOR
+		       .implode(DIRECTORY_SEPARATOR, array_slice($pathBits, 1))
+		       .".php";
+
+		if (!file_exists($path))
+			throw new ClassNotFoundException("Class file at path \"$path\" does not exist.");
+		else
+			include_once $path;
+	}
+
+}
