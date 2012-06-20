c6bf2c234865fdbaaf8d5480d9fffa32130c7d24
Cyril Mengin <cyril@fc-designs.com>
2012-06-20 17:16:17 +0200

    Added the ArgumentParser which will take care of command line arguments, and should clean up the code quite a lot.

---
+++ lib/iceberg/shell/ArgumentParser.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace iceberg\shell;
+
+use iceberg\shell\exceptions\InvalidArgumentPassedException;
+
+class ArgumentParser {
+
+	private $arguments = array();
+	private static $object = false;
+
+	public static function getInstance() {
+		if (!static::$object)
+			static::$object = new ArgumentParser();
+
+		return static::$object;
+	}
+
+	private function __construct() {
+		global $argv;
+		global $argc;
+
+		$parsedArguments = array();
+
+		for ($i = 1; $i < $argc; $i++) {
+		
+			if ( substr($argv[$i], 0, 2) == "--" ) {
+				$keyword = str_replace("-", "_", substr($argv[$i], 2));
+	
+				if ( $i+1 != $argc && substr($argv[$i+1], 0, 2) != "--" )
+					$value = $argv[++$i];
+				else
+					$value = true;
+					
+				$parsedArguments[$keyword] = $value;
+			} else 
+				throw new InvalidArgumentPassedException("Invalid argument \"{$argv[$i]}\" passed.");
+		}
+		
+		$this->arguments = $parsedArguments;
+	}
+	
+	public function __get($name) {
+		
+		if (substr($name, -4) != "_val") {
+		
+			if ( array_key_exists($name, $this->arguments) )
+				return $this->arguments[$name];
+			else 
+				return false;
+	
+		} else {
+			$name = substr($name, 0, -4);		
+
+			if ( array_key_exists($name, $this->arguments) )	
+				return is_string($this->arguments[$name]) ? $this->arguments[$name] : false;
+			else 
+				return false;
+		}
+	}
+
+}
+
---
+++ lib/iceberg/shell/exceptions/InvalidArgumentPassedException.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace iceberg\shell\exceptions;
+
+class InvalidArgumentPassedException extends \Exception { }
