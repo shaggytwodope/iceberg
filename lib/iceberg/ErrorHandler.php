7b8c27c2baa5b2f149e285e2548c3e1a391e6336
Cyril Mengin <cyril@fc-designs.com>
2012-06-23 18:24:57 +0200

    Moved the logging code to an ErrorHandler::log() method instead of the exception handler itself.

--- lib/iceberg/ErrorHandler.php
+++ lib/iceberg/ErrorHandler.php
@@ -6,7 +6,7 @@ class ErrorHandler {
 
 	private static $registered = false;
 	private static $logPath = false;
-	
+
 	public static function register($logPath = false) {
 		if (!static::$registered)
 			static::$registered = set_exception_handler(__CLASS__."::exceptionHandler");
@@ -19,21 +19,28 @@ class ErrorHandler {
 		
 		return static::$registered;
 	}
-	
-	public static function exceptionHandler($exception) {
+
+	public static function log($name, $message, $log = false) {
 
 		$logMessage = "[".date(DATE_RFC822)."]";
-		$logMessage .= " ".get_class($exception)." ";
+		$logMessage .= " ".$name." ";
 		$logMessage .= ":";
-		$logMessage .= $exception->getMessage();
+		$logMessage .= $message;
 		$logMessage .= PHP_EOL;
-	
-		if (static::$logPath != false) {
+		
+		if (static::$logPath && $log) 
 			fwrite(static::$logPath, $logMessage);
+		
+		echo $message . PHP_EOL;
+	}
+
+	public static function exceptionHandler($exception) {
+		static::log( get_class($exception), $exception->getMessage(), true );
+
+		if (static::$logPath) 
 			fclose(static::$logPath);
-		}
-	
-		exit($exception->getMessage() . PHP_EOL);
+
+		exit;
 	}
 
 }
